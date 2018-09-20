f134ca3690afd339348c0bd0e94482ea16473545
hffelix <39575629+hffelix@users.noreply.github.com>
2018-09-19 17:06:32 -0700

    Burn-the-rope

---
+++ 426/Assets/Scripts/TouchMove.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+ 
+public class TouchMove : MonoBehaviour {
+
+	public Camera m_CameraTwo;
+     
+    // Use this for initialization
+    void Start ()
+    {
+        StartCoroutine(OnMouseDown());
+    }
+ 
+    IEnumerator OnMouseDown()
+    {
+        
+        Vector3 screenSpace = m_CameraTwo.WorldToScreenPoint(transform.position);
+       
+        Vector3 offset = transform.position - m_CameraTwo.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z));
+        while (Input.GetMouseButton(0))
+        {
+            Vector3 curScreenSpace = new Vector3(Input.mousePosition.x, Input.mousePosition.y, screenSpace.z);
+    
+            Vector3 curPosition = m_CameraTwo.ScreenToWorldPoint(curScreenSpace) + offset;
+        
+            transform.position = curPosition;
+            yield return new WaitForFixedUpdate(); 
+        }
+    }
+}
