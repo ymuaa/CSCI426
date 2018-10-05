7e63f6f3a0e2e848c8b3705fc2c3a6d6517d8a9a
hffelix <39575629+hffelix@users.noreply.github.com>
2018-10-05 15:07:10 -0700

    NewUIDemo

---
+++ SecertLight/Assets/Scripts/get.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class get : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void OnCollisionEnter(Collision aaa) //aaa為自定義碰撞事件
+    {    
+        if (aaa.gameObject.name == "CubeA") //如果aaa碰撞事件的物件名稱是CubeA
+        {    
+            print("OK"); //在除錯視窗中顯示OK
+        }
+    }
+}
